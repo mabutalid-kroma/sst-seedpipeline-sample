71b1eb00616279d1042f8d4e5c4ac5d8db05f79f
mabutalid-kroma <marco.butalid@kroma.ph>
2023-03-31 12:52:49 +0800

    add frontend

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -3,6 +3,6 @@ import { Time } from "@sst-seed-sample/core/time";
 
 export const handler = ApiHandler(async (_evt) => {
   return {
-    body: `Hello world. The time is ${Time.now()}`,
+    body: `Hello Marco. The time is ${Time.now()}`,
   };
 });
